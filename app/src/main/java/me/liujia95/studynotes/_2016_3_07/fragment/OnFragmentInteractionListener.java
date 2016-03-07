42e4a281dfdaad64dbeb5bad2fc90fe618d6187f
liujia95 <liujia95me@126.com>
2016-03-07 13:40:45 +0800

    2016.3.07——仿微信tabs

---
+++ app/src/main/java/me/liujia95/studynotes/_2016_3_07/fragment/OnFragmentInteractionListener.java
@@ -0,0 +1,10 @@
+package me.liujia95.studynotes._2016_3_07.fragment;
+
+import android.net.Uri;
+
+/**
+ * Created by Administrator on 2015/10/19.
+ */
+public interface OnFragmentInteractionListener {
+    public void onFragmentInteraction(Uri uri);
+}
--- app/src/main/java/me/liujia95/studynotes/activity/MainActivity.java
+++ app/src/main/java/me/liujia95/studynotes/activity/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         mItems.add("2016.3.03 首字母分类索引");
         mItems.add("2016.3.04 未读消息小红点");
         mItems.add("2016.3.06 自定义组合控件");
+        mItems.add("2016.3.07 仿微信Tab bar");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mItems);
         mListview.setAdapter(adapter);
@@ -77,6 +78,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             case 4:
                 Intent intent4 = new Intent(this, me.liujia95.studynotes._2016_3_06.activity.MainActivity.class);
                 startActivity(intent4);
+                break;
+            case 5:
+                Intent intent5 = new Intent(this, me.liujia95.studynotes._2016_3_07.MainActivity.class);
+                startActivity(intent5);
                 break;
         }
     }
